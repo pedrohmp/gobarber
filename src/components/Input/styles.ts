f6762459f828c64542a4fa2a5cfb5bc69d05ce74
Pedro Porta <pedrohmp@icloud.com>
2020-07-07 22:01:48 -0300

    gobarber fix

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
-  height: 60px;
   padding: 0 16px;
   background: #232129;
   border-radius: 10px;
   margin-bottom: 8px;
+  height: 60px;
 `;
 
 export const TextInput = styled.TextInput`
